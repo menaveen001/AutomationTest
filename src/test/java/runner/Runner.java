eac5ef20916002b77043ec6dde79e0c371482a85
menaveen001 <menaveenpal@gmail.com>
2023-05-03 17:49:09 +0530

    add testng more feature

--- src/test/java/runner/Runner.java
+++ src/test/java/runner/Runner.java
@@ -3,19 +3,12 @@ package runner;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
-@CucumberOptions(
-		features ="src/test/java/features/LogOut.feature",
-		glue ="stepdefinitions",
-	//	plugin = {
-			//	"html:target/cucumber-html-report.html","pretty"
-				
-		//}
-		plugin = {"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"}
-	
-		
-		)
+@CucumberOptions(features = "src/test/java/features/LogOut.feature", glue = "stepdefinitions",
+
+		plugin = { "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
+				"html:target/cucumber-html-report.html", "pretty" }
+
+)
 public class Runner extends AbstractTestNGCucumberTests {
 
-	
-	
 }
